a3be3fb4fb9b0f2706b4b12e527777353a6b519b
SaileshBK <sailesh.bk1@icloud.com>
2024-08-11 01:21:40 -0500

    feat: Updated README.md to use programming quotes api

---
+++ data/endpoints.mjs
@@ -0,0 +1,3 @@
+
+export const advice_api = "https://api.adviceslip.com/advice";
+export const programming_quotes_api = 'https://github.com/skolakoda/programming-quotes-api/raw/master/Data/quotes.json';
---
+++ index.mjs
@@ -0,0 +1,44 @@
+import fetch from 'node-fetch';
+import Mustache from 'mustache';
+import fs from 'fs';
+import * as Constants from './data/endpoints.mjs';
+const Mustache_Main_Dir = './main.mustache';
+
+/**
+ * Picks a random item from an array.
+ * @param {Array<Object>} array - The array from which to pick a random item.
+ * @returns {Object} - A randomly selected item from the array.
+ */
+const getRandomItem = (array) => {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
+
+/**
+ * Fetches data from the API.
+ * @returns json.
+ */
+const fetchData = async () => {
+    const response = await fetch(Constants.programming_quotes_api)
+        .then(result => result.json())
+    return getRandomItem(response);
+}
+
+/**
+ * It fetches the data from the API, then it reads the template file, then it renders the template with
+ * the data, then it writes the rendered template to the README.md file.
+ */
+const generateReadMe = async () => {
+    const fetchDataResponse = await fetchData();
+    const quoteData = {
+        quote: fetchDataResponse.en,
+        author: fetchDataResponse.author
+    };
+
+    fs.readFile(Mustache_Main_Dir, (err, data) => {
+        if (err) throw err;
+        const output = Mustache.render(data.toString(), quoteData);
+        fs.writeFileSync('README.md', output);
+    });
+}
+generateReadMe();
